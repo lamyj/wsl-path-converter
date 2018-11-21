932d1bdbb78b0f48a5f8db77fbb30ceeaa95357d
Julien Lamy <lamy@unistra.fr>
2018-11-21 12:38:15 +0100

    Add setup and tox

---
+++ setup.py
@@ -0,0 +1,55 @@
+from codecs import open
+import glob
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="wsl-path-converter",
+    version="0.2.0",
+    
+    description="Convert between Linux and Windows path in WSL",
+    long_description=long_description,
+    
+    url="https://github.com/lamyj/wsl-path-converter",
+    
+    author="Julien Lamy",
+    author_email="lamy@unistra.fr",
+    
+    license="MIT",
+    
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        
+        "Environment :: Console",
+        
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        
+        "Topic :: Communications :: File Sharing",
+        "Topic :: System :: Archiving :: Mirroring",
+        
+        "License :: OSI Approved :: MIT License",
+        
+        "Operating System :: Microsoft :: Windows",
+        
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        
+        "Topic :: System :: Filesystems",
+    ],
+    
+    keywords="wsl, windows, linux, path",
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    
+    entry_points={ "console_scripts": [ "wpc=wsl_path_converter.main:main"] },
+)
